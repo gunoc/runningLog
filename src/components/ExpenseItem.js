c033362838caca275c54404413ccf9d269f2789c
JH <vvaa277@gmail.com>
2022-09-22 23:16:38 +0900

    test

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,6 +12,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount} km</div>
       </div>
+      <button>수정</button>
     </Card>
   );
 }
