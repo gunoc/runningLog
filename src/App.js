8d39d5b72ef093b5e47cd0ec1cb91a42f9d69b08
JH <vvaa277@gmail.com>
2022-09-22 22:23:06 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Expenses from "./components/Expenses";
+
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "카보로딩의 효과",
+      amount: 6.2,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "겨울 러닝 대비",
+      amount: 5.3,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "러닝화 바꾸고 싶다",
+      amount: 5.7,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "컨디션 최상!",
+      amount: 6.3,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>러닝일지</h2>
+      
+      <Expenses items={expenses}/>
     </div>
   );
 }
